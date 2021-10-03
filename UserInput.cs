d5247f38bf2fae206bcd4ef0c15b8a3f86535c8a
Tejassonar <sonartejas908@gmail.com>
2021-10-03 11:48:16 +0530

    Taking inputs from user

---
+++ UserInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day19_Regex_Assignment
+{
+    class UserInput
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string email { get; set; }
+        public string mobileNum { get; set; }
+        public string password { get; set; }
+    }
+}
