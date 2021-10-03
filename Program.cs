c1ee14d28a966d8f112b79d7593c7a095e473d45
Tejassonar <sonartejas908@gmail.com>
2021-10-03 11:47:08 +0530

    Contains main method

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day19_Regex_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods.FirstNameInput();
+            Methods.LastNameInput();
+            Methods.EmailInput();
+            Methods.MobileNumInput();
+            Methods.PasswordInput();
+            
+            
+
+
+        }
+    }
+}
